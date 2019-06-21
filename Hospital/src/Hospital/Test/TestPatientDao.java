e715906ddeeca65257f2c0447a626a9adba75943
Moline <45853149+Moline-x@users.noreply.github.com>
2019-06-21 09:26:57 +0800

    Update TestPatientDao.java

--- Hospital/src/Hospital/Test/TestPatientDao.java
+++ Hospital/src/Hospital/Test/TestPatientDao.java
@@ -10,8 +10,34 @@ import Hospital.pojo.Patient;
 public class TestPatientDao {
 	
 	patientDao dao = new patientDao();
-	
 	@Test
+	public void testInsert() {
+		Patient patient = new Patient("刘建雄", 19, "男", "18712345678");
+		dao.insert(patient);
+	}
+	/*@Test
+	public void testUpdate() {
+		Patient patient = new Patient(20,"留学",20,"男","18742015656");
+		dao.update(patient);
+	}*/
+	/*@Test
+	public void testDelete() {
+		dao.delete(4);
+		System.out.println("success");
+	}
+	@Test
+	public void testQueryById() {
+		Patient patient = dao.queryById(1);
+		System.out.println(patient);
+	}
+	@Test
+	public void testQuery() {
+		ArrayList<Patient> list = dao.query();
+		
+		for(Patient patient : list) {
+			System.out.println(patient);
+		}
+	}*/
 	
 
 }
